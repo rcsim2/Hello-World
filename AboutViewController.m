31d1e0f3e3b78a5c599ca1f029d52dc6d661f716
RG <rgosens2@gmailcom>
2020-02-26 10:41:27 +0100

    first commit

---
+++ AboutViewController.m
@@ -0,0 +1,9 @@
+//
+//  AboutViewController.m
+//  Hello World
+//
+//  Created by Rik Goossens on 09/02/2020.
+//  Copyright © 2020 Rik Goossens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
